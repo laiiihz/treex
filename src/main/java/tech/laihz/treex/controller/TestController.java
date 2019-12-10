e8dc599165ea76626a8aaf1d40003a1735642028
laiiihz@outlook.com <laihongzhan1123@gmail.com>
2019-12-10 17:40:02 +0800

    fix

--- src/main/java/tech/laihz/treex/controller/TestController.java
+++ src/main/java/tech/laihz/treex/controller/TestController.java
@@ -5,16 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import tech.laihz.treex.ResponseBo;
+import tech.laihz.treex.utils.RMap;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TestController {
-  @RequestMapping(value = "/test", method = RequestMethod.GET)
-  public ResponseBo Test() {
-    List<ResponseBo> list = new ArrayList<ResponseBo>();
-    list.add(ResponseBo.error(500, ""));
-    return ResponseBo.list(200, "fawefawef", 100, list);
+  @RequestMapping(value = "/api/test", method = RequestMethod.GET)
+  public RMap Test() {
+    return RMap.success(RMap.SUCCESS,"success");
+  }
+
+  @GetMapping(value = "/api/check-connection")
+  public RMap CheckConnection() {
+    return RMap.success(RMap.SUCCESS,"success");
   }
 }
